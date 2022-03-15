5dcb66ee493eb7cfd5b7b50c569568bf876e8a66
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-03-15 14:13:29 +0300

    Add layout components

--- src/app/admin/layout/layout.module.ts
+++ src/app/admin/layout/layout.module.ts
@@ -1,15 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LayoutComponent } from '../layout/layout.component';
+import { ComponentsModule } from './components/components.module';
+import { HeaderComponent } from './components/header/header.component';
 
 
 
 @NgModule({
   declarations: [
-    LayoutComponent
+    LayoutComponent,
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    ComponentsModule
   ]
 })
 export class LayoutModule { }
