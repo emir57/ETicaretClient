83417c39490e3ddcc588acd455f4d590f1558e83
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-03-20 02:02:07 +0300

    refactoring

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './admin/components/dashboard/dashboard.component';
 import { LayoutComponent } from './admin/layout/layout.component';
 
 const routes: Routes = [
   {path:"admin",component:LayoutComponent,children:[
+    {path:"",component:DashboardComponent},
     {path:"customers",loadChildren:()=> import("./admin/components/customer/customer.module").then(module=>module.CustomerModule)},
     {path:"products",loadChildren:()=> import("./admin/components/products/products.module").then(module=>module.ProductsModule)},
     {path:"orders",loadChildren:()=> import("./admin/components/order/order.module").then(module=>module.OrderModule)},
-    {path:"dashboard",loadChildren:()=> import("./admin/components/dashboard/dashboard.module").then(module=>module.DashboardModule)},
   ]}
 ];
 
