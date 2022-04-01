3ad9c1ae8dbdfe8fcf0981a41c7538a4e7420b63
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-04-01 15:59:01 +0300

    success position

--- src/app/admin/layout/layout.component.ts
+++ src/app/admin/layout/layout.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { AlertifyService, MessageType } from 'src/app/services/admin/alertify.service';
+import { AlertifyService, MessageType, Position } from 'src/app/services/admin/alertify.service';
 
 @Component({
   selector: 'app-layout',
