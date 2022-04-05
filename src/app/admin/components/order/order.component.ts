ee235cb6573f902cb6c72209c680dc1042c7fe42
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-04-05 16:41:21 +0300

    OrderComponent in showSpinner

--- src/app/admin/components/order/order.component.ts
+++ src/app/admin/components/order/order.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { BaseComponent, SpinnerType } from 'src/app/base/base.component';
 
 @Component({
   selector: 'app-order',
   templateUrl: './order.component.html',
   styleUrls: ['./order.component.css']
 })
-export class OrderComponent implements OnInit {
+export class OrderComponent extends BaseComponent implements OnInit {
 
-  constructor() { }
+  constructor(public spinner: NgxSpinnerService) { super(spinner) }
 
   ngOnInit(): void {
+    this.showSpinner(SpinnerType.BallSpinClockwise, 500)
   }
 
 }
