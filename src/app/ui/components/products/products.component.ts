697ed826e0d8983b6db2e219491705da902ae65f
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-04-11 16:45:17 +0300

    get httpClientService

--- src/app/ui/components/products/products.component.ts
+++ src/app/ui/components/products/products.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { BaseComponent, SpinnerType } from 'src/app/base/base.component';
+import { HttpClientService } from 'src/app/services/common/http-client.service';
 
 @Component({
   selector: 'app-products',
@@ -9,7 +10,7 @@ import { BaseComponent, SpinnerType } from 'src/app/base/base.component';
 })
 export class ProductsComponent extends BaseComponent implements OnInit {
 
-  constructor(public spinner: NgxSpinnerService) {
+  constructor(public spinner: NgxSpinnerService, private httpClientService: HttpClientService) {
     super(spinner);
   }
 
