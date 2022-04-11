72cb4367aafa39fdfe5a126e590dc35cc0c34682
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-04-11 16:34:37 +0300

    Inject baseUrl

--- src/app/services/common/http-client.service.ts
+++ src/app/services/common/http-client.service.ts
@@ -1,11 +1,12 @@
-import { Injectable } from '@angular/core';
+import { Inject, Injectable } from '@angular/core';
 import { HttpClient } from "@angular/common/http";
 @Injectable({
   providedIn: 'root'
 })
 export class HttpClientService {
 
-  constructor(private httpClient: HttpClient) { }
+  constructor(private httpClient: HttpClient,
+    @Inject("baseUrl") private baseUrl: string) { }
 
   get<T>() {
 
