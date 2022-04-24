1eb42c7ff91311dd74be3a8a544022f98dbd6598
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-04-24 17:51:25 +0300

    extends BaseComponent

--- src/app/admin/components/products/create/create.component.ts
+++ src/app/admin/components/products/create/create.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { BaseComponent } from 'src/app/base/base.component';
 import { Create_Product } from 'src/app/contracts/create_product';
 import { ProductService } from 'src/app/services/common/models/product.service';
 
@@ -7,17 +9,20 @@ import { ProductService } from 'src/app/services/common/models/product.service';
   templateUrl: './create.component.html',
   styleUrls: ['./create.component.css']
 })
-export class CreateComponent implements OnInit {
+export class CreateComponent extends BaseComponent implements OnInit {
 
   constructor(
+    public spinner: NgxSpinnerService,
     private productService: ProductService
-  ) { }
+  ) {
+    super(spinner)
+  }
 
   ngOnInit(): void {
   }
 
   create(name: HTMLInputElement, stock: HTMLInputElement, price: HTMLInputElement) {
-    const create_product:Create_Product = new Create_Product();
+    const create_product: Create_Product = new Create_Product();
     create_product.name = name.value;
     create_product.stock = +stock.value;
     create_product.price = +price.value;
