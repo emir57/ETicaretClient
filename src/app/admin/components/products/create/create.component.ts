5bb7f2c4963e5a77dbef6992e4972d8f6667c77f
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-04-24 17:04:04 +0300

    change selector name

--- src/app/admin/components/products/create/create.component.ts
+++ src/app/admin/components/products/create/create.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-create',
+  selector: 'product-create',
   templateUrl: './create.component.html',
   styleUrls: ['./create.component.css']
 })
