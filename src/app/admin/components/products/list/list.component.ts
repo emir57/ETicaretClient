9967b315c2413500b41c8ffb72ea1fc46c9bc2cf
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-04-30 15:18:07 +0300

    extends BaseComponent

--- src/app/admin/components/products/list/list.component.ts
+++ src/app/admin/components/products/list/list.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { BaseComponent } from 'src/app/base/base.component';
 import { List_Product } from 'src/app/contracts/list_product';
 import { ProductService } from 'src/app/services/common/models/product.service';
 
@@ -9,15 +11,26 @@ import { ProductService } from 'src/app/services/common/models/product.service';
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.css']
 })
-export class ListComponent implements OnInit {
+export class ListComponent extends BaseComponent implements OnInit {
 
   displayedColumns: string[] = ['name', 'stock', 'price', 'createdDate', 'updatedDate'];
   dataSource: MatTableDataSource<List_Product> = new MatTableDataSource;
   constructor(
+    public spinner: NgxSpinnerService,
     private productService: ProductService
-  ) { }
+  ) {
+    super(spinner);
+  }
 
   ngOnInit(): void {
+    this
+    this.productService.read(
+      (value) => {
+
+      },
+      (error) => {
+
+      })
   }
 
 }
