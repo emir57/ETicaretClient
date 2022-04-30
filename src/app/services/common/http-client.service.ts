76abba6d18f8fd3967050b27c8ac020dda7e27ab
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-04-30 16:34:08 +0300

    delete and put add queryString

--- src/app/services/common/http-client.service.ts
+++ src/app/services/common/http-client.service.ts
@@ -40,7 +40,8 @@ export class HttpClientService {
     if (requestParameter.fullEndPoint) {
       url = requestParameter.fullEndPoint;
     } else {
-      url = `${this.url(requestParameter)}`;
+      url = `${this.url(requestParameter)}${requestParameter.queryString ?
+        "?" + requestParameter.queryString : ""}`;
     }
     return this.httpClient.put<T>(url, body, { headers: requestParameter.headers });
   }
@@ -49,7 +50,8 @@ export class HttpClientService {
     if (requestParameter.fullEndPoint) {
       url = requestParameter.fullEndPoint;
     } else {
-      url = `${this.url(requestParameter)}?id=${id}`;
+      url = `${this.url(requestParameter)}?id=${id}${requestParameter.queryString ?
+        "?" + requestParameter.queryString : ""}`;
     }
     return this.httpClient.delete<T>(url, { headers: requestParameter.headers });
   }
