3a0b90676b11902415b633ab2103873c26f8f6a3
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-04-30 17:07:49 +0300

    createdProduct emit

--- src/app/admin/components/products/create/create.component.ts
+++ src/app/admin/components/products/create/create.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit, Type } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output, Type } from '@angular/core';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { BaseComponent, SpinnerType } from 'src/app/base/base.component';
 import { Create_Product } from 'src/app/contracts/create_product';
 import { AlertifyService, MessageType, Position } from 'src/app/services/admin/alertify.service';
 import { ProductService } from 'src/app/services/common/models/product.service';
+
 
 @Component({
   selector: 'product-create',
@@ -22,6 +23,7 @@ export class CreateComponent extends BaseComponent implements OnInit {
 
   ngOnInit(): void {
   }
+  @Output() createdProduct: EventEmitter<Create_Product> = new EventEmitter();
 
   create(name: HTMLInputElement, stock: HTMLInputElement, price: HTMLInputElement) {
     this.showSpinner(SpinnerType.BallSpinClockwise);
@@ -34,7 +36,8 @@ export class CreateComponent extends BaseComponent implements OnInit {
       this.alertifyService.message("Ürün başarıyla eklenmiştir.", {
         messageType: MessageType.Success,
         position: Position.TopRight
-      })
+      });
+      this.createdProduct.emit(create_product);
     }, (errorMessage) => {
       this.alertifyService.message(errorMessage, {
         messageType: MessageType.Error,
