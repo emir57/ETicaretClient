4e47fd97f1a2a79923958610334c11dedde6b4ad
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-04-30 15:16:18 +0300

    inject productService in list component

--- src/app/admin/components/products/list/list.component.ts
+++ src/app/admin/components/products/list/list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
 import { List_Product } from 'src/app/contracts/list_product';
+import { ProductService } from 'src/app/services/common/models/product.service';
 
 @Component({
   selector: 'product-list',
@@ -12,7 +13,9 @@ export class ListComponent implements OnInit {
 
   displayedColumns: string[] = ['name', 'stock', 'price', 'createdDate', 'updatedDate'];
   dataSource: MatTableDataSource<List_Product> = new MatTableDataSource;
-  constructor() { }
+  constructor(
+    private productService: ProductService
+  ) { }
 
   ngOnInit(): void {
   }
