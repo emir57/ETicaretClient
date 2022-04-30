7655b82be90738d975a2d1a0278471438dcac76d
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-04-30 15:08:02 +0300

    read function

--- src/app/services/common/models/product.service.ts
+++ src/app/services/common/models/product.service.ts
@@ -1,6 +1,7 @@
 import { HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Create_Product } from 'src/app/contracts/create_product';
+import { List_Product } from 'src/app/contracts/list_product';
 import { HttpClientService } from '../http-client.service';
 
 @Injectable({
@@ -31,5 +32,15 @@ export class ProductService {
         errorCallBack(message);
       }
     })
+  }
+
+  async read(): Promise<List_Product[]> {
+    const promiseData: Promise<List_Product[]> = this.httpClientService.get<List_Product[]>({
+      controller: "products",
+      action: "getall"
+    }).toPromise();
+    promiseData.then()
+      .catch();
+    return await promiseData;
   }
 }
