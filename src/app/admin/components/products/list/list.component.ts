d3510fd7d1e55b785a49990c9eb04c2927573b3a
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-05-08 17:32:32 +0300

    add edit column

--- src/app/admin/components/products/list/list.component.ts
+++ src/app/admin/components/products/list/list.component.ts
@@ -14,7 +14,7 @@ import { ProductService } from 'src/app/services/common/models/product.service';
 })
 export class ListComponent extends BaseComponent implements OnInit {
   @ViewChild(MatPaginator) paginator!: MatPaginator;
-  displayedColumns: string[] = ['name', 'stock', 'price', 'createdDate', 'updatedDate', 'delete'];
+  displayedColumns: string[] = ['name', 'stock', 'price', 'createdDate', 'updatedDate', 'edit', 'delete'];
   dataSource: MatTableDataSource<List_Product> = new MatTableDataSource;
   constructor(
     public spinner: NgxSpinnerService,
