4441cf050f69bee095f867c1f80a3f4501505aa1
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-05-08 17:53:18 +0300

    injects

--- src/app/directives/admin/delete.directive.ts
+++ src/app/directives/admin/delete.directive.ts
@@ -1,10 +1,15 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, Renderer2 } from '@angular/core';
+import { HttpClientService } from 'src/app/services/common/http-client.service';
 
 @Directive({
   selector: '[appDelete]'
 })
 export class DeleteDirective {
 
-  constructor() { }
+  constructor(
+    private element: ElementRef,
+    private _renderer: Renderer2,
+    private httpClientService: HttpClientService
+  ) { }
 
 }
