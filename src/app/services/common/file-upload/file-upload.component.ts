cb54354c27c1e88fbdf8be8a63069ce25420040a
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-05-11 16:51:09 +0300

    delete if and else

--- src/app/services/common/file-upload/file-upload.component.ts
+++ src/app/services/common/file-upload/file-upload.component.ts
@@ -18,37 +18,29 @@ export class FileUploadComponent implements OnInit {
   dropped(files: NgxFileDropEntry[]) {
     this.files = files;
     for (const droppedFile of files) {
+      const fileEntry = droppedFile.fileEntry as FileSystemFileEntry;
+      fileEntry.file((file: File) => {
 
-      // Is it a file?
-      if (droppedFile.fileEntry.isFile) {
-        const fileEntry = droppedFile.fileEntry as FileSystemFileEntry;
-        fileEntry.file((file: File) => {
+        // Here you can access the real file
+        console.log(droppedFile.relativePath, file);
 
-          // Here you can access the real file
-          console.log(droppedFile.relativePath, file);
+        /**
+        // You could upload it like this:
+        const formData = new FormData()
+        formData.append('logo', file, relativePath)
 
-          /**
-          // You could upload it like this:
-          const formData = new FormData()
-          formData.append('logo', file, relativePath)
+        // Headers
+        const headers = new HttpHeaders({
+          'security-token': 'mytoken'
+        })
 
-          // Headers
-          const headers = new HttpHeaders({
-            'security-token': 'mytoken'
-          })
+        this.http.post('https://mybackend.com/api/upload/sanitize-and-save-logo', formData, { headers: headers, responseType: 'blob' })
+        .subscribe(data => {
+          // Sanitized logo returned from backend
+        })
+        **/
 
-          this.http.post('https://mybackend.com/api/upload/sanitize-and-save-logo', formData, { headers: headers, responseType: 'blob' })
-          .subscribe(data => {
-            // Sanitized logo returned from backend
-          })
-          **/
-
-        });
-      } else {
-        // It was a directory (empty directories are added, otherwise only files)
-        const fileEntry = droppedFile.fileEntry as FileSystemDirectoryEntry;
-        console.log(droppedFile.relativePath, fileEntry);
-      }
+      });
     }
   }
 
