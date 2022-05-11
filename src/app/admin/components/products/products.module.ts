40ba47fa6e30f7c1a87bf34fc853f615a7c8a85f
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-05-11 16:43:24 +0300

    use app-file-upload

--- src/app/admin/components/products/products.module.ts
+++ src/app/admin/components/products/products.module.ts
@@ -13,6 +13,7 @@ import { MatPaginatorModule } from '@angular/material/paginator';
 import { DeleteDirective } from 'src/app/directives/admin/delete.directive';
 import { DeleteDialogComponent } from 'src/app/dialogs/delete-dialog/delete-dialog.component';
 import { MatDialogActions, MatDialogModule } from '@angular/material/dialog';
+import { FileUploadModule } from 'src/app/services/common/file-upload/file-upload.module';
 
 
 @NgModule({
@@ -34,7 +35,8 @@ import { MatDialogActions, MatDialogModule } from '@angular/material/dialog';
     MatButtonModule,
     MatTableModule,
     MatPaginatorModule,
-    MatDialogModule
+    MatDialogModule,
+    FileUploadModule
   ]
 })
 export class ProductsModule { }
