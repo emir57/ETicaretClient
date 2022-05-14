f21a91491da545b3391f4c74f8c0da1a6d438b86
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-05-14 17:03:33 +0300

    refactoring delete directive

--- src/app/directives/admin/delete.directive.ts
+++ src/app/directives/admin/delete.directive.ts
@@ -3,6 +3,7 @@ import { Directive, ElementRef, EventEmitter, HostListener, Input, Output, Rende
 import { MatDialog } from '@angular/material/dialog';
 import { DeleteDialogComponent, DeleteState } from 'src/app/dialogs/delete-dialog/delete-dialog.component';
 import { AlertifyService, MessageType } from 'src/app/services/admin/alertify.service';
+import { DialogService } from 'src/app/services/common/dialog/dialog.service';
 import { HttpClientService } from 'src/app/services/common/http-client.service';
 import { ProductService } from 'src/app/services/common/models/product.service';
 declare var $: any;
@@ -20,7 +21,8 @@ export class DeleteDirective {
     private _renderer: Renderer2,
     private httpClientService: HttpClientService,
     private alertifyService: AlertifyService,
-    private dialog: MatDialog
+    private dialog: MatDialog,
+    private dialogService: DialogService
   ) {
     const i: HTMLElement = _renderer.createElement("i");
     i.setAttribute("class", "bi bi-trash3 text-danger");
@@ -29,37 +31,30 @@ export class DeleteDirective {
   }
   @HostListener("click")
   async onClick() {
-    this.openDialog(() => {
-      const i: HTMLTableCellElement = this.element.nativeElement;
-      this.httpClientService.delete({
-        controller: this.controller,
-        action: "delete"
-      }, this.id).subscribe((data: any) => {
-        this.alertifyService.message(data.message, { messageType: MessageType.Success })
-        $(i.parentElement).animate({
-          opacity: 0,
-          left: "+=50",
-          height: "toogle"
-        }, 700, () => {
-          this.callBack.emit();
-        })
-      }, (responseErr: HttpErrorResponse) => {
-        this.alertifyService.message("Ürün silinirken bir hata meydana geldi.", { messageType: MessageType.Error })
-      });
-    });
-  }
-
-  openDialog(afterClosed: any): void {
-    const dialogRef = this.dialog.open(DeleteDialogComponent, {
-      width: '250px',
-      data: DeleteState.Yes,
-    });
-
-    dialogRef.afterClosed().subscribe(result => {
-      if (result == DeleteState.Yes) {
-        afterClosed();
+    this.dialogService.openDialog({
+      componentType: DeleteDialogComponent,
+      data: DeleteState,
+      options: {
+        width: "250px"
+      },
+      afterClosed: () => {
+        const i: HTMLTableCellElement = this.element.nativeElement;
+        this.httpClientService.delete({
+          controller: this.controller,
+          action: "delete"
+        }, this.id).subscribe((data: any) => {
+          this.alertifyService.message(data.message, { messageType: MessageType.Success })
+          $(i.parentElement).animate({
+            opacity: 0,
+            left: "+=50",
+            height: "toogle"
+          }, 700, () => {
+            this.callBack.emit();
+          })
+        }, (responseErr: HttpErrorResponse) => {
+          this.alertifyService.message("Ürün silinirken bir hata meydana geldi.", { messageType: MessageType.Error })
+        });
       }
-    });
+    })
   }
-
 }
