5daaaaacf745b04e204fb7afff646ebf4d335c89
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-05-14 16:45:11 +0300

    dialog service openDialog

--- src/app/services/common/dialog/dialog.service.ts
+++ src/app/services/common/dialog/dialog.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DialogService {
 
-  constructor() { }
+  constructor(
+    private dialog: MatDialog
+  ) { }
+
+  openDialog(afterClosed: any): void {
+    const dialogRef = this.dialog.open(FileUploadDialogComponent, {
+      width: '250px',
+      data: FileUploadDialogState.Yes,
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      if (result == FileUploadDialogState.Yes) {
+        afterClosed();
+      }
+    });
+  }
 }
