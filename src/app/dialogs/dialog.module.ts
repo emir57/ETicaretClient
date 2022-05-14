f4d051ae3f34518348a8ef4ff36b0b4198e387ab
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-05-14 15:56:22 +0300

    declare and exports dialog components

--- src/app/dialogs/dialog.module.ts
+++ src/app/dialogs/dialog.module.ts
@@ -1,12 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { DeleteDialogComponent } from './delete-dialog/delete-dialog.component';
+import { FileUploadDialogComponent } from './file-upload-dialog/file-upload-dialog.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    DeleteDialogComponent,
+    FileUploadDialogComponent
+  ],
   imports: [
     CommonModule
+  ],
+  exports: [
+    DeleteDialogComponent,
+    FileUploadDialogComponent
   ]
 })
 export class DialogModule { }
