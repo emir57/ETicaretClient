e1f91d9c8020acb778ae98e8a744ffa698455467
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-05-14 15:44:47 +0300

    super

--- src/app/dialogs/file-upload-dialog/file-upload-dialog.component.ts
+++ src/app/dialogs/file-upload-dialog/file-upload-dialog.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialogRef } from '@angular/material/dialog';
+import { BaseDialog } from '../base/base-dialog';
 
 @Component({
   selector: 'app-file-upload-dialog',
   templateUrl: './file-upload-dialog.component.html',
   styleUrls: ['./file-upload-dialog.component.css']
 })
-export class FileUploadDialogComponent implements OnInit {
+export class FileUploadDialogComponent extends BaseDialog<FileUploadDialogComponent> {
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    dialogRef: MatDialogRef<FileUploadDialogComponent>
+  ) {
+    super(dialogRef);
   }
-
 }
