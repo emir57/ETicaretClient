8f2b04a0374606f39767ac9da23e799ea993297e
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-05-14 15:42:01 +0300

    dialogRef

--- src/app/dialogs/delete-dialog/delete-dialog.component.ts
+++ src/app/dialogs/delete-dialog/delete-dialog.component.ts
@@ -1,19 +1,22 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { BaseDialog } from '../base/base-dialog';
 
 @Component({
   selector: 'app-delete-dialog',
   templateUrl: './delete-dialog.component.html',
   styleUrls: ['./delete-dialog.component.css']
 })
-export class DeleteDialogComponent implements OnInit {
+export class DeleteDialogComponent extends BaseDialog<DeleteDialogComponent> {
 
   constructor(
-    public dialogRef: MatDialogRef<DeleteDialogComponent>,
+    dialogRef: MatDialogRef<DeleteDialogComponent>,
     @Inject(MAT_DIALOG_DATA) public data: DeleteState,
-  ) { }
-  ngOnInit(): void {
+  ) {
+    super(dialogRef);
   }
+
+
   close() {
     this.data = DeleteState.No;
     this.dialogRef.close();
