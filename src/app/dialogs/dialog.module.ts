b6115cf8a17b5bc01b8f29da3dec225ac223193d
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-05-27 17:50:20 +0300

    Create selectProductImageDialog Component

--- src/app/dialogs/dialog.module.ts
+++ src/app/dialogs/dialog.module.ts
@@ -4,13 +4,15 @@ import { DeleteDialogComponent } from './delete-dialog/delete-dialog.component';
 import { FileUploadDialogComponent } from './file-upload-dialog/file-upload-dialog.component';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatButtonModule } from '@angular/material/button';
+import { SelectProductImageDialogComponent } from './select-product-image-dialog/select-product-image-dialog.component';
 
 
 
 @NgModule({
   declarations: [
     DeleteDialogComponent,
-    FileUploadDialogComponent
+    FileUploadDialogComponent,
+    SelectProductImageDialogComponent
   ],
   imports: [
     CommonModule,
---
+++ src/app/dialogs/select-product-image-dialog/select-product-image-dialog.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-select-product-image-dialog',
+  templateUrl: './select-product-image-dialog.component.html',
+  styleUrls: ['./select-product-image-dialog.component.css']
+})
+export class SelectProductImageDialogComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
