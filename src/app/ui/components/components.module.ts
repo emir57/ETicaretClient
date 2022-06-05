19cf4e10adf822ee40c3e78792fc6ce76cbc324a
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-06-05 17:26:12 +0300

    fix RegisterComponent problem

--- src/app/ui/components/components.module.ts
+++ src/app/ui/components/components.module.ts
@@ -3,13 +3,10 @@ import { CommonModule } from '@angular/common';
 import { BasketsModule } from './baskets/baskets.module';
 import { HomeModule } from './home/home.module';
 import { ProductsModule } from './products/products.module';
-import { RegisterComponent } from './register/register.component';
-
-
 
 @NgModule({
   declarations: [
-    RegisterComponent
+
   ],
   imports: [
     CommonModule,
