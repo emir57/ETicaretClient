86407286939bc545c32c8adaa6176f5d9b6dcda3
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-06-05 17:25:19 +0300

    Create register component and module

--- src/app/ui/components/components.module.ts
+++ src/app/ui/components/components.module.ts
@@ -3,11 +3,14 @@ import { CommonModule } from '@angular/common';
 import { BasketsModule } from './baskets/baskets.module';
 import { HomeModule } from './home/home.module';
 import { ProductsModule } from './products/products.module';
+import { RegisterComponent } from './register/register.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    RegisterComponent
+  ],
   imports: [
     CommonModule,
     BasketsModule,
