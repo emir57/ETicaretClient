bc29c79ba683e1380d6e44012dbf2b1dd087840f
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-06-11 18:27:04 +0300

    Create_User contracts

--- src/app/services/common/models/user.service.ts
+++ src/app/services/common/models/user.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { User } from 'src/app/entities/user';
+import { HttpClientService } from '../http-client.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  constructor(
+    private httpClientService: HttpClientService
+  ) { }
+
+  create(user: User) {
+    this.httpClientService.post({
+      controller: "users"
+    }, user)
+  }
 }
