0f51aa51334efaf1b6f7967b27112b5661edf2d0
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-06-12 14:02:15 +0300

    createLoginForm

--- src/app/ui/components/login/login.component.ts
+++ src/app/ui/components/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -14,6 +14,18 @@ export class LoginComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
+    this.createLoginForm();
+  }
+
+  createLoginForm() {
+    this.loginForm = this.formBuilder.group({
+      username: ["", [
+        Validators.required,
+        Validators.maxLength(50), Validators.minLength(3)]],
+      password: ["", [
+        Validators.required,
+        Validators.minLength(6)]]
+    })
   }
 
 }
