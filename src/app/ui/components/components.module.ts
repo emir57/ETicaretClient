ed9bffe8c345f30962eed90e6d271a8678a20d53
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-06-12 13:55:37 +0300

    Create login module

--- src/app/ui/components/components.module.ts
+++ src/app/ui/components/components.module.ts
@@ -3,7 +3,6 @@ import { CommonModule } from '@angular/common';
 import { BasketsModule } from './baskets/baskets.module';
 import { HomeModule } from './home/home.module';
 import { ProductsModule } from './products/products.module';
-import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [],
