fa87b6f262b4dd1d7ba403f381e2a4d28ee7bf41
emir57 <emir.gurbuz06@hotmail.com>
2022-06-21 14:42:44 +0300

    app component -> isAuthenticated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { ToastrService } from 'ngx-toastr';
+import { AuthService } from './services/common/auth.service';
 import { CustomToastrService, ToastrMessageType, ToastrPosition } from './services/ui/custom-toastr.service';
 declare var $: any;
 @Component({
@@ -9,9 +10,9 @@ declare var $: any;
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ETicaretClient';
-  constructor() {
 
+  constructor(public authService: AuthService) {
+    authService.identityCheck();
   }
 }
 
