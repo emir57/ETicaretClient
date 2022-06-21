73d9bb745ec3b84186b40edc33cab432e1531a63
emir57 <emir.gurbuz06@hotmail.com>
2022-06-21 13:42:55 +0300

    admin path use AuthGuard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './admin/components/dashboard/dashboard.component';
 import { LayoutComponent } from './admin/layout/layout.component';
+import { AuthGuard } from './guards/common/auth.guard';
 import { HomeComponent } from './ui/components/home/home.component';
 
 const routes: Routes = [
@@ -11,7 +12,8 @@ const routes: Routes = [
       { path: "customers", loadChildren: () => import("./admin/components/customer/customer.module").then(module => module.CustomerModule) },
       { path: "products", loadChildren: () => import("./admin/components/products/products.module").then(module => module.ProductsModule) },
       { path: "orders", loadChildren: () => import("./admin/components/order/order.module").then(module => module.OrderModule) },
-    ]
+    ],
+    canActivate: [AuthGuard]
   },
   { path: "", component: HomeComponent },
   { path: "basket", loadChildren: () => import("./ui/components/baskets/baskets.module").then(module => module.BasketsModule) },
