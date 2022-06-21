c6d6d4686795beefafdca259a6d03d32254014f0
emir57 <emir.gurbuz06@hotmail.com>
2022-06-21 14:12:42 +0300

    show and hide spinner

--- src/app/guards/common/auth.guard.ts
+++ src/app/guards/common/auth.guard.ts
@@ -1,7 +1,10 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { JwtHelperService } from '@auth0/angular-jwt';
+import { NgxSpinnerService } from 'ngx-spinner';
 import { Observable } from 'rxjs';
+import { SpinnerType } from 'src/app/base/base.component';
+import { CustomToastrService, ToastrMessageType, ToastrPosition } from 'src/app/services/ui/custom-toastr.service';
 
 @Injectable({
   providedIn: 'root'
@@ -9,15 +12,32 @@ import { Observable } from 'rxjs';
 export class AuthGuard implements CanActivate {
 
   constructor(
-    private jwtHelper: JwtHelperService
+    private jwtHelper: JwtHelperService,
+    private router: Router,
+    private toastrService: CustomToastrService,
+    private spinner: NgxSpinnerService
   ) { }
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+    this.spinner.show(SpinnerType.BallPulseAsync);
     const token: string = localStorage.getItem("accessToken") as string;
-    const decodeToken = this.jwtHelper.decodeToken(token);
-    const expirationDate: Date | null = this.jwtHelper.getTokenExpirationDate(token);
-    const isExpired: boolean = this.jwtHelper.isTokenExpired(token);
-    debugger;
+
+    //const decodeToken = this.jwtHelper.decodeToken(token);
+    //const expirationDate: Date | null = this.jwtHelper.getTokenExpirationDate(token);
+    let isExpired: boolean;
+    try {
+      isExpired = this.jwtHelper.isTokenExpired(token);
+    } catch {
+      isExpired = true;
+    }
+    if (!token || isExpired) {
+      this.router.navigate(["login"], { queryParams: { returnUrl: state.url } });
+      this.toastrService.message("Oturum açmanız gerekiyor", "Yetkisiz Erişim", {
+        messageType: ToastrMessageType.Warning,
+        position: ToastrPosition.TopRight
+      })
+    }
+    this.spinner.hide(SpinnerType.BallPulseAsync);
     return true;
   }
 
