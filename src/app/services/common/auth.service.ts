4e21bbe47f6991090702a4cbd289128b6ad6ddc3
emir57 <emir.gurbuz06@hotmail.com>
2022-06-21 14:32:09 +0300

    export let _isAuthenticated

--- src/app/services/common/auth.service.ts
+++ src/app/services/common/auth.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import { JwtHelperService } from '@auth0/angular-jwt';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(
+    private jwtHelper: JwtHelperService
+  ) { }
+
+  identityCheck() {
+    const token: string = localStorage.getItem("accessToken") as string;
+    let isExpired: boolean;
+    try {
+      isExpired = this.jwtHelper.isTokenExpired(token);
+    } catch {
+      isExpired = true;
+    }
+  }
 }
+
+export let _isAuthenticated: boolean;
