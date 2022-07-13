10f201a6a2d365e75190369817e18353b6f9e968
emir57 <emir.gurbuz06@hotmail.com>
2022-07-13 14:22:58 +0300

    facebookLogin function signIn

--- src/app/ui/components/login/login.component.ts
+++ src/app/ui/components/login/login.component.ts
@@ -1,4 +1,4 @@
-import { SocialAuthService, SocialUser } from '@abacritt/angularx-social-login';
+import { FacebookLoginProvider, SocialAuthService, SocialUser } from '@abacritt/angularx-social-login';
 import { Component, OnInit } from '@angular/core';
 import { UntypedFormBuilder, UntypedFormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
@@ -59,7 +59,7 @@ export class LoginComponent extends BaseComponent implements OnInit {
   }
 
   async facebookLogin() {
-
+    this.socialAuthService.signIn(FacebookLoginProvider.PROVIDER_ID);
   }
 
   async onSubmit(value: any) {
